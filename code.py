167b6d38d3c350c170189535f406b6afa6323092
Rakesh Kedar <53192600+rakeshkedar181@users.noreply.github.com>
2019-09-26 15:39:24 +0530

    Added task descriptions.

--- code.py
+++ code.py
@@ -1,5 +1,9 @@
-# --------------
+                                            # Project Name :- Olympic Hero
+    
+# Task 1:- Data Loading
+# Task Description:- Let's start with the simple task of loading the data and do a little bit of renaming.
 #Importing header files
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,17 +13,13 @@ path
 
 #Code starts here
 data = pd.read_csv(path)
-
 data.rename(columns={'Total':'Total_Medals'},inplace=True)
-
 data.head(10)
 
-
-# --------------
+# Task 2:- Summer or Winter
+# Task Description:- Some Countries love Summer, some Winter. We think it has to do something with their Olympic performance.
+#                    For this task we will try to figure out which olympic event does a country perform better in.
 #Code starts here
-
-
-
 
 data['Better_Event'] = np.where(data['Total_Summer']>data['Total_Winter'],'Summer','Winter')
 
@@ -27,21 +27,15 @@ data['Better_Event'] = np.where(data['Total_Summer'] == data['Total_Winter'],'Bo
 
 better_event = data['Better_Event'].value_counts().idxmax()
 
-
 print(better_event)
 
-
-
-
-# --------------
-#Code starts here
-# Task Description
-
-# Top 10
+# Task 3:- Top 10
+# Task Description:- 
 # So we figured out which is a better event for each country. Let's move on to finding out the best performing countries across all events
 # In this task we will try to find
 # 1) Which are the top 10 performing teams at summer event (with respect to total medals),   winter event and overall?
 # 2) How many teams are present in all of the three lists above?
+#Code starts here
 
 top_countries = data[['Country_Name','Total_Summer','Total_Winter','Total_Medals']]
 
@@ -61,17 +55,13 @@ common = list(set(top_10_summer) & set(top_10_winter) & set(top_10))
 
 print(common)
 
-
-# --------------
-#Code starts here
-
-# Plotting Top 10
-# From the lists that you have created from the previous task, let's plot the medal count of the top 10 countries for better visualisation
+# Task 4:-Plotting Top 10
+# Task Description:- From the lists that you have created from the previous task, 
+#                    let's plot the medal count of the top 10 countries for better visualisation\
+# Code starts here
 
 summer_df = data[data['Country_Name'].isin(top_10_summer)]
-
 winter_df = data[data['Country_Name'].isin(top_10_winter)]
-
 top_df = data[data['Country_Name'].isin(top_10)]
 
 # print(summer_df)
@@ -96,12 +86,11 @@ plt.ylabel('Total of medals in summer season')
 plt.xticks(rotation=45)
 plt.show()
 
-
-# --------------
-#Code starts here
 # Task 5 :  Top performing country(Gold)
 # Task Description : Winning silver or bronze medals is a big achievement but winning gold is bigger.
 # Using the above created dataframe subsets, in this task let's find out which country has had the best performance with respect to the ratio between gold medals won and total medals won.
+# --------------
+#Code starts here
 
 summer_df['Golden_Ratio'] = summer_df['Gold_Summer'] / summer_df['Total_Summer']
 summer_max_ratio = max(summer_df['Golden_Ratio'])
@@ -119,12 +108,9 @@ print(summer_country_gold)
 print(winter_country_gold)
 print(top_country_gold)
 
-
-
-# --------------
-#Code starts here
 # Task 6 :- Best in the world
 # Task Description :- Winning Gold is great but is winning most gold equivalent to being the best overall perfomer? Let's find out.
+#Code starts here
 
 # dropping last row as it contains total of all values calculated vertically
 data_1 = data.drop(data.tail(1).index)
@@ -142,21 +128,12 @@ print(most_points)
 
 print(best_country)
 
-
-
-
-# --------------
-#Code starts here
-
 # Task 7 :- Plot for the best
 # Task Description :- We know which country is best when it comes to winning the most points in Olympic Games. Let's plot the medal count to visualise their success better
+# Code starts here
 
 best = data[data['Country_Name'] == best_country]
-
-# print(best)
-
 best = best[['Gold_Total','Silver_Total','Bronze_Total']]
-
 print(best)
 
 best.plot(kind='bar',stacked=True)
